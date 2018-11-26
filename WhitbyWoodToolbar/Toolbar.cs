bbf77e366c1b6827d89a298ef28114fdaaae7439
Alex Baalham <alex.baalham@virginmedia.com>
2018-11-26 18:00:15 +0000

    1st commit

---
+++ WhitbyWoodToolbar/Toolbar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace WhitbyWoodToolbar
+{
+    public class Toolbar : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            application.CreateRibbonTab("Whitby Wood");
+
+            Uri wwLogo = new Uri("pack://application:,,,/WhitbyWoodToolbar;component/resources/Link.png");
+            var panel1 = application.CreateRibbonPanel("Whitby Wood", "General panel");
+            panel1.AddItem(new PushButtonData(
+                "WW Standards",
+                "WW",
+                assemblyPath,
+                "WhitbyWoodToolbar.WWStandards")
+            {
+                ToolTip = "Link to WW BIM standards",
+                LargeImage = new BitmapImage(wwLogo)
+            });
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ WhitbyWoodToolbar/WWStandards.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WhitbyWoodToolbar
+{
+    [Transaction(TransactionMode.Manual)]
+    public class WWStandards : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            MessageBox.Show("This would take you to WW BIM Standards");
+            return Result.Succeeded;
+        }
+    }
+}
